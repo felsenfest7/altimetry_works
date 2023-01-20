cac56fa6ee3a8db05342d3d8bb9ce1c2a629d92d
felsenfest7 <92101782+felsenfest7@users.noreply.github.com>
2023-01-20 10:03:08 +0300

    Add files via upload

--- plot.py
+++ plot.py
@@ -13,9 +13,12 @@ import os
 import datetime
 from matplotlib.dates import DayLocator, HourLocator, DateFormatter, drange, MonthLocator, YearLocator
 import statsmodels
+import matplotlib.dates as dates
+from sklearn.metrics import mean_squared_error
 
+#HER AYRI PLOT BAŞLIĞI İÇİN FONKSİYONLARA TİTLE KOMUTLARI YAZILACAK
 
-def plot_ssh(df):
+def plot_ssh(df, title):
     """
         --> Bu fonksiyonun amacı girdi olarak girilen herhangi bir ALES/LRM/SAR/SARIN df'inin ssh plotunu çizdirmektir.
 
@@ -29,26 +32,54 @@ def plot_ssh(df):
     fig, ax = plt.subplots()
     ax.plot_date(df["cdate_t"], df["ssh.55"], "royalblue", label="Deniz Seviyesi Yüksekliği")
 
-    #Moving average hesabı
+    #MOVING AVERAGE HESABI
     ##Moving average ile daha smooth bir veri elde ediliyor, ekstren değerler ihmal ediliyor.
     df["EWMA7"] = df["ssh.55"].ewm(span = 7).mean()
     ax.plot_date(df["cdate_t"], df["EWMA7"], "red", label="Kayan Ortalama")
 
-    #Sadede year bilgileri için YearLocator kullanılmalı
+    #Sadece year bilgileri için YearLocator kullanılmalı
     ax.xaxis.set_major_locator(YearLocator())
     #Burada ise verinin veri tipinin formatı girilmeli
     ax.fmt_xdata = DateFormatter('% Y-% m-% d')
 
     #Geriye kalan bilgiler. Bu kodun bitimi ile ssh plotu çizimi biter.
     ax.set_xlabel("Tarih", fontsize=13)
-    ax.set_ylabel("Deniz Seviyesi Yüksekliği (m)", fontsize=13)
+    ax.set_ylabel("Günlük Deniz Seviyesi Yüksekliği (m)", fontsize=13)
+
+    #TREND EĞRİSİ HESAPLAMA VE ÇİZDİRME
+    ## İlk olarak datetime objectler matplotlib için çevriliyor
+    x_date = df["cdate_t"]
+    x_num = dates.date2num(x_date)
+    ##Trend eğrisi hesaplanıyor. Birinci dereceden yapılıyor.
+    trend = np.polyfit(x_num, df["ssh.55"], 1)
+    fit = np.poly1d(trend)
+    ##Veriler tekrar datetime'a çevriliyor
+    x_fit = np.linspace(x_num.min(), x_num.max())
+    ax.plot_date(dates.num2date(x_fit), fit(x_fit), "k--", label = "Trend")
+
+    #MEAN SQUARE ERROR HESABI
+    ##Ortalama SSH değeri
+    ort_ssh = df["ssh.55"].mean()
+    ort_ssh = "%.3f" % ort_ssh
+    ##Ortalama Karesel Hata Hesabı (MSE)
+    mse = mean_squared_error(df["ssh.55"], df["mssh.05"])   #Bu değer metre biriminde
+    mse_mm = mse * 1000     #Bu değer milimetre biriminde
+    mse_mm = "%.2f" % mse_mm
+
+    #Trend değerlerinin plota yazdırılması
+    plt.text(datetime.date(2007, 6, 1), 24, f"Trend: {ort_ssh} m ± {mse_mm} mm")
+
+    plt.title(title)
     ax.legend(loc="best")
+    plt.grid(True)
     plt.show()
 
-def plot_ssh_yillik(df):
+def plot_ssh_yillik(df, title):
     """
         --> Bu fonksiyonun amacı girdi olarak girilen herhangi bir ALES/LRM/SAR/SARIN df'inin ssh plotunu çizdirmektir.
-        --> Aylık fonksiyonu ile elde edilen veriler için geçerli.
+        --> Yıllık fonksiyonu ile elde edilen veriler için geçerli.
+        --> Trend hesabı yapılarak ardından MSE hesabı yapılır. MSE hesabı yapılırken SLA = SSH -MSS eşitliği kullanılır.
+        --> Önceki fonksiyonlarda datetime object değiştirildiği için integer olarak kullanılıyor.
 
             input: dataframe
             output: plot
@@ -59,15 +90,38 @@ def plot_ssh_yillik(df):
     #Ardından dfx çizdirilirken sorun verdiği için dff diye yeni bir dataframe e kopyalanır
     dff = dfx
 
-    #Plotların çizdirilmesi ve gerekli bilgilerinin yazılması
-    plt.plot(dff["cdate_t"], dff["ssh.55"], label = "Deniz Seviyesi Yüksekliği")
-    plt.title("Title")
-    plt.xlabel("Yıl")
-    plt.ylabel("Deniz Seviyesi Yüksekliği (m)")
+    #Plotun çizdirilmesi
+    fig, ax = plt.subplots()
+    ax.plot(dff["cdate_t"], dff["ssh.55"], "royalblue", label="Deniz Seviyesi Yüksekliği")
+
+    # Geriye kalan bilgiler. Bu kodun bitimi ile ssh plotu çizimi biter.
+    ax.set_xlabel("Yıl", fontsize=13)
+    ax.set_ylabel("Ortalama Yıllık Deniz Seviyesi Yüksekliği (m)", fontsize=13)
+
+    #TREND EĞRİSİ HESAPLAMA VE ÇİZDİRME
+    ##Trend eğrisi hesaplanıyor. Birinci dereceden yapılıyor.
+    trend = np.polyfit(dff["cdate_t"], dff["ssh.55"], 1)
+    fit = np.poly1d(trend)
+    ax.plot(dff["cdate_t"], fit(dff["cdate_t"]), "k--", label="Trend")
+
+    #MEAN SQUARE ERROR HESABI
+    ##Ortalama SSH değeri
+    ort_ssh = dff["ssh.55"].mean()
+    ort_ssh = "%.3f" % ort_ssh
+    ##Ortalama Karesel Hata Hesabı (MSE)
+    mse = mean_squared_error(dff["ssh.55"], dff["mssh.05"])  # Bu değer metre biriminde
+    mse_mm = mse * 1000  # Bu değer milimetre biriminde
+    mse_mm = "%.2f" % mse_mm
+
+    #Trend değerlerinin plota yazdırılması
+    plt.text(2002, 25.15, f"Trend: {ort_ssh} m ± {mse_mm} mm")
+
+    plt.title(title)
     plt.legend(loc = "best")
+    plt.grid(True)
     plt.show()
 
-def plot_ssh_aylik(df):
+def plot_ssh_aylik(df, title):
     """
         --> Bu fonksiyonun amacı girdi olarak girilen herhangi bir ALES/LRM/SAR/SARIN df'inin ssh plotunu çizdirmektir.
         --> Aylık fonksiyonu ile elde edilen veriler için geçerli.
@@ -84,16 +138,45 @@ def plot_ssh_aylik(df):
     fig, ax = plt.subplots()
     ax.plot_date(dff["cdate_t"], dff["ssh.55"], "royalblue", label="Deniz Seviyesi Yüksekliği")
 
-    # Sadede year bilgileri için YearLocator kullanılmalı
+    #MOVING AVERAGE HESABI
+    ##Moving average ile daha smooth bir veri elde ediliyor, ekstren değerler ihmal ediliyor.
+    dff["EWMA3"] = dff["ssh.55"].ewm(span=3).mean()
+    ax.plot_date(dff["cdate_t"], dff["EWMA3"], "red", label="Kayan Ortalama")
+
+    #Year-Month bilgileri için MonthLocator kullanılmalı
     ax.xaxis.set_major_locator(MonthLocator(interval = 6))
-    # Burada ise verinin veri tipinin formatı girilmeli
+    #Burada ise verinin veri tipinin formatı girilmeli
     ax.fmt_xdata = DateFormatter('% Y-% m-% d')
 
-    ax.set_xlabel("Tarih", fontsize=13)
-    ax.set_ylabel("Deniz Seviyesi Yüksekliği (m)", fontsize=13)
-    ax.legend(loc="best")
-    plt.show()
+    #TREND EĞRİSİ HESAPLAMA VE ÇİZDİRME
+    ## İlk olarak datetime objectler matplotlib için çevriliyor
+    x_date = dff["cdate_t"]
+    x_num = dates.date2num(x_date)
+    ##Trend eğrisi hesaplanıyor. Birinci dereceden yapılıyor.
+    trend = np.polyfit(x_num, dff["ssh.55"], 1)
+    fit = np.poly1d(trend)
+    ##Veriler tekrar datetime'a çevriliyor
+    x_fit = np.linspace(x_num.min(), x_num.max())
+    ax.plot_date(dates.num2date(x_fit), fit(x_fit), "k--", label="Trend")
 
+    # MEAN SQUARE ERROR HESABI
+    ##Ortalama SSH değeri
+    ort_ssh = dff["ssh.55"].mean()
+    ort_ssh = "%.3f" % ort_ssh
+    ##Ortalama Karesel Hata Hesabı (MSE)
+    mse = mean_squared_error(dff["ssh.55"], dff["mssh.05"])  # Bu değer metre biriminde
+    mse_mm = mse * 1000  # Bu değer milimetre biriminde
+    mse_mm = "%.2f" % mse_mm
+
+    #Trend değerlerinin plota yazdırılması
+    plt.text(datetime.date(2001, 12, 1), 24.55, f"Trend: {ort_ssh} m ± {mse_mm} mm")
+
+    ax.set_xlabel("Tarih", fontsize=13)
+    ax.set_ylabel("Ortalama Aylık Deniz Seviyesi Yüksekliği (m)", fontsize=13)
+    ax.legend(loc="best")
+    plt.grid(True)
+    plt.title(title)
+    plt.show()
 
 
 

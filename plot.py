8e28c565b81178d27a879a70fc7c7ca86b7b9fb3
felsenfest7 <92101782+felsenfest7@users.noreply.github.com>
2023-01-30 01:20:04 +0300

    Add files via upload

--- plot.py
+++ plot.py
@@ -18,9 +18,12 @@ from sklearn.metrics import mean_squared_error
 import plotly.express as px
 import plotly.graph_objects as go
 from matplotlib.ticker import FormatStrFormatter
+import read_merge_nc as rmn
+from dateutil.relativedelta import relativedelta
+import math as m
 
 #PLOTLARIN ÇİZİMİ
-
+#-----------------------------------------------------------------------------------------------------------------------
 def plot_ssh_gunluk(df, title):
     """
         --> Bu fonksiyonun amacı girdi olarak girilen herhangi bir ALES/LRM/SAR/SARIN df'inin ssh plotunu çizdirmektir.
@@ -33,6 +36,7 @@ def plot_ssh_gunluk(df, title):
 
     #Ardından normal bir fig, ax ile grafik çizdirilir
     fig, ax = plt.subplots()
+    ax.yaxis.set_major_formatter(FormatStrFormatter('%.2f'))
     ax.plot_date(df["cdate_t"], df["ssh.55"], "royalblue", label="Deniz Seviyesi Yüksekliği")
 
     #MOVING AVERAGE HESABI
@@ -65,18 +69,17 @@ def plot_ssh_gunluk(df, title):
     ort_ssh = df["ssh.55"].mean()
     ort_ssh = "%.3f" % ort_ssh
     ##Ortalama Karesel Hata Hesabı (MSE)
-    mse = mean_squared_error(df["ssh.55"], df["mssh.05"])   #Bu değer metre biriminde
-    mse_cm = mse  * 100   #Bu değer santimetre biriminde
-    mse_cm = "%.2f" % mse_cm
+    mse = rmn.mean_square_gunluk(df)
+    mse = "%.2f" % mse
 
     #Trend değerlerinin plota yazdırılması
-    plt.text(datetime.date(2018, 1, 1), 25, f"Trend: {ort_ssh} m ± {mse_cm} cm")
+    plt.text(datetime.date(2004, 6, 1), 37.56, f"Trend: {ort_ssh} m ± {mse} mm/yıl")
 
     plt.title(title)
     ax.legend(loc="best")
     plt.grid(True)
     plt.show()
-
+#-----------------------------------------------------------------------------------------------------------------------
 def plot_ssh_aylik(df, title):
     """
         --> Bu fonksiyonun amacı girdi olarak girilen herhangi bir ALES/LRM/SAR/SARIN df'inin ssh plotunu çizdirmektir.
@@ -95,6 +98,7 @@ def plot_ssh_aylik(df, title):
 
     #Plotun çizdirilmesi
     fig, ax = plt.subplots()
+    ax.yaxis.set_major_formatter(FormatStrFormatter('%.2f'))
     ax.plot_date(dff["cdate_t"], dff["ssh.55"], "royalblue", label="Deniz Seviyesi Yüksekliği")
 
     #MOVING AVERAGE HESABI
@@ -118,24 +122,23 @@ def plot_ssh_aylik(df, title):
     ort_ssh = dff["ssh.55"].mean()
     ort_ssh = "%.3f" % ort_ssh
     ##Ortalama Karesel Hata Hesabı (MSE)
-    mse = mean_squared_error(dff["ssh.55"], dff["mssh.05"])  # Bu değer metre biriminde
-    mse_cm = mse * 100  # Bu değer santimetre biriminde
-    mse_cm = "%.2f" % mse_cm
+    mse = rmn.mean_square_aylik(df)
+    mse = "%.2f" % mse
 
     #Trend değerlerinin plota yazdırılması
-    plt.text(datetime.date(2018, 1, 1), 35, f"Trend: {ort_ssh} m ± {mse_cm} cm")
+    plt.text(datetime.date(2004, 6, 1), 37.56, f"Trend: {ort_ssh} m ± {mse} mm/yıl")
 
     # Year-Month bilgileri için MonthLocator kullanılmalı
-    ax.xaxis.set_major_locator(MonthLocator(interval=18))
+    ax.xaxis.set_major_locator(MonthLocator(interval=36))
     # Burada ise verinin veri tipinin formatı girilmeli
     ax.fmt_xdata = DateFormatter('% Y-% m-% d')
-    ax.set_xlabel("Tarih", fontsize=13)
+    ax.set_xlabel("Tarih (Yıl-Ay)", fontsize=13)
     ax.set_ylabel("Ortalama Aylık Deniz Seviyesi Yüksekliği (m)", fontsize=13)
     ax.legend(loc="best")
     plt.grid(True)
     plt.title(title)
     plt.show()
-
+#-----------------------------------------------------------------------------------------------------------------------
 def plot_ssh_yillik(df, title):
     """
         --> Bu fonksiyonun amacı girdi olarak girilen herhangi bir ALES/LRM/SAR/SARIN df'inin ssh plotunu çizdirmektir.
@@ -155,6 +158,7 @@ def plot_ssh_yillik(df, title):
     #Plotun çizdirilmesi
     fig, ax = plt.subplots()
     ax.locator_params(integer=True)
+    ax.yaxis.set_major_formatter(FormatStrFormatter('%.2f'))
     ax.plot(dff["cdate_t"], dff["ssh.55"], "royalblue", label="Deniz Seviyesi Yüksekliği")
 
     # Geriye kalan bilgiler. Bu kodun bitimi ile ssh plotu çizimi biter.
@@ -177,13 +181,13 @@ def plot_ssh_yillik(df, title):
     mse_cm = "%.2f" % mse_cm
 
     #Trend değerlerinin plota yazdırılması
-    plt.text(2018, 40, f"Trend: {ort_ssh} m ± {mse_cm} cm")
+    plt.text(2010, 25.6, f"Trend: {ort_ssh} m ± {mse_cm} cm")
 
     plt.title(title)
     plt.legend(loc = "best")
     plt.grid(True)
     plt.show()
-
+#-----------------------------------------------------------------------------------------------------------------------
 def plot_sla_gunluk(df, title):
     """
         --> Bu fonksiyonun amacı girdi olarak girilen herhangi bir ALES/LRM/SAR/SARIN df'inin SLA plotunu çizdirmektir.
@@ -207,6 +211,28 @@ def plot_sla_gunluk(df, title):
     #Burada ise verinin veri tipinin formatı girilmeli
     ax.fmt_xdata = DateFormatter('% Y-% m-% d')
 
+    # TREND EĞRİSİ HESAPLAMA VE ÇİZDİRME
+    ## İlk olarak datetime objectler matplotlib için çevriliyor
+    x_date = df["cdate_t"]
+    x_num = dates.date2num(x_date)
+    ##Trend eğrisi hesaplanıyor. Birinci dereceden yapılıyor.
+    trend = np.polyfit(x_num, df["sla"], 1)
+    fit = np.poly1d(trend)
+    ##Veriler tekrar datetime'a çevriliyor
+    x_fit = np.linspace(x_num.min(), x_num.max())
+    ax.plot_date(dates.num2date(x_fit), fit(x_fit), "k--", label="Trend")
+
+    # MEAN SQUARE ERROR HESABI
+    ##Ortalama SSH değeri
+    ort_sla = df["sla"].mean()
+    ort_sla = "%.3f" % ort_sla #m
+    ##Ortalama Karesel Hata Hesabı (MSE)
+    mse = rmn.mean_square_gunluk(df)
+    mse = "%.2f" % mse
+
+    # Trend değerlerinin plota yazdırılması
+    plt.text(datetime.date(2004, 6, 1), -0.38, f"Trend: {ort_sla} m ± {mse} mm/yıl")
+
     #Geriye kalan bilgiler. Bu kodun bitimi ile ssh plotu çizimi biter.
     ax.set_xlabel("Yıl", fontsize=13)
     ax.set_ylabel("Günlük Deniz Seviyesi Anomalisi (m)", fontsize=13)
@@ -215,7 +241,7 @@ def plot_sla_gunluk(df, title):
     ax.legend(loc="best")
     plt.grid(True)
     plt.show()
-
+#-----------------------------------------------------------------------------------------------------------------------
 def plot_sla_aylik(df, title):
     """
         --> Bu fonksiyonun amacı girdi olarak girilen herhangi bir ALES/LRM/SAR/SARIN df'inin SLA plotunu çizdirmektir.
@@ -247,13 +273,35 @@ def plot_sla_aylik(df, title):
     #Burada ise verinin veri tipinin formatı girilmeli
     ax.fmt_xdata = DateFormatter('% Y-% m-% d')
 
+    # TREND EĞRİSİ HESAPLAMA VE ÇİZDİRME
+    ## İlk olarak datetime objectler matplotlib için çevriliyor
+    x_date = dff["cdate_t"]
+    x_num = dates.date2num(x_date)
+    ##Trend eğrisi hesaplanıyor. Birinci dereceden yapılıyor.
+    trend = np.polyfit(x_num, dff["sla"], 1)
+    fit = np.poly1d(trend)
+    ##Veriler tekrar datetime'a çevriliyor
+    x_fit = np.linspace(x_num.min(), x_num.max())
+    ax.plot_date(dates.num2date(x_fit), fit(x_fit), "k--", label="Trend")
+
+    # MEAN SQUARE ERROR HESABI
+    ##Ortalama SSH değeri
+    ort_sla = dff["sla"].mean()
+    ort_sla = "%.3f" % ort_sla
+    ##Ortalama Karesel Hata Hesabı (MSE)
+    mse = rmn.mean_square_aylik(df)
+    mse = "%.2f" % mse
+
+    # Trend değerlerinin plota yazdırılması
+    plt.text(datetime.date(2004, 1, 1), -0.18, f"Trend: {ort_sla} m ± {mse} mm/yıl")
+
     ax.set_xlabel("Tarih", fontsize=13)
     ax.set_ylabel("Ortalama Aylık Deniz Seviyesi Anomalisi (m)", fontsize=13)
     ax.legend(loc="best")
     plt.grid(True)
     plt.title(title)
     plt.show()
-
+#-----------------------------------------------------------------------------------------------------------------------
 def plot_sla_yillik(df, title):
     """
         --> Bu fonksiyonun amacı girdi olarak girilen herhangi bir ALES/LRM/SAR/SARIN df'inin SLA plotunu çizdirmektir.
@@ -282,3 +330,285 @@ def plot_sla_yillik(df, title):
     plt.legend(loc = "best")
     plt.grid(True)
     plt.show()
+#-----------------------------------------------------------------------------------------------------------------------
+def sla_gunluk(df, title):
+    """
+        --> Bu fonksiyonun amacı girdi olarak girilen herhangi bir SLA df'inin SLA plotunu çizdirmektir.
+
+        input: dataframe
+        output: plot
+    """
+
+    #Ardından normal bir fig, ax ile grafik çizdirilir
+    fig, ax = plt.subplots()
+    ax.yaxis.set_major_formatter(FormatStrFormatter('%.2f'))
+    ax.plot_date(df["cdate_t"], df["sla.40"], "royalblue", label="Deniz Seviyesi Anomalisi")
+
+    #MOVING AVERAGE HESABI
+    ##Moving average ile daha smooth bir veri elde ediliyor, ekstren değerler ihmal ediliyor.
+    df["EWMA7"] = df["sla.40"].ewm(span = 7).mean()
+    ax.plot_date(df["cdate_t"], df["EWMA7"], "red", label="Hareketli Ortalama")
+
+    #Sadece year bilgileri için YearLocator kullanılmalı
+    ax.xaxis.set_major_locator(YearLocator())
+    #Burada ise verinin veri tipinin formatı girilmeli
+    ax.fmt_xdata = DateFormatter('% Y-% m-% d')
+
+    # TREND EĞRİSİ HESAPLAMA VE ÇİZDİRME
+    ## İlk olarak datetime objectler matplotlib için çevriliyor
+    x_date = df["cdate_t"]
+    x_num = dates.date2num(x_date)
+    ##Trend eğrisi hesaplanıyor. Birinci dereceden yapılıyor.
+    trend = np.polyfit(x_num, df["sla.40"], 1)
+    fit = np.poly1d(trend)
+    ##Veriler tekrar datetime'a çevriliyor
+    x_fit = np.linspace(x_num.min(), x_num.max())
+    ax.plot_date(dates.num2date(x_fit), fit(x_fit), "k--", label="Trend")
+
+    # MEAN SQUARE ERROR HESABI
+    ##Ortalama SSH değeri
+    ort_sla = df["sla.40"].mean()
+    ort_sla = "%.3f" % ort_sla  # m
+    ##Ortalama Karesel Hata Hesabı (MSE)
+    mse = rmn.mean_square_gunluk_lrm(df)
+    mse = "%.2f" % mse
+
+    # Trend değerlerinin plota yazdırılması
+    plt.text(datetime.date(2019, 1, 1), -0.44, f"Trend: {ort_sla} m ± {mse} mm/yıl")
+
+    #Geriye kalan bilgiler. Bu kodun bitimi ile ssh plotu çizimi biter.
+    ax.set_xlabel("Yıl", fontsize=13)
+    ax.set_ylabel("Günlük Deniz Seviyesi Anomalisi (m)", fontsize=13)
+
+    plt.title(title)
+    ax.legend(loc="best")
+    plt.grid(True)
+    plt.show()
+#-----------------------------------------------------------------------------------------------------------------------
+def sla_aylik(df, title):
+    """
+        --> Bu fonksiyonun amacı girdi olarak girilen herhangi bir SLA df'inin SLA plotunu çizdirmektir.
+        --> Aylık fonksiyonu ile elde edilen veriler için geçerli.
+
+            input: dataframe
+            output: plot
+    """
+    # İlk önce cdate_t değerleri indexe gittiği için ve çizilirken sorun yaşandığı için index resetlenir
+    dfx = df.reset_index()
+    # Ardından dfx çizdirilirken sorun verdiği için dff diye yeni bir dataframe e kopyalanır
+    dff = dfx
+
+    #Nan değerlerinin alınmaması
+    dff = dff[dff["sla.40"].notna()]
+
+    #Plotun çizdirilmesi
+    fig, ax = plt.subplots()
+    ax.yaxis.set_major_formatter(FormatStrFormatter('%.2f'))
+    ax.plot_date(dff["cdate_t"], dff["sla.40"], "royalblue", label="Deniz Seviyesi Anomalisi")
+
+    # MOVING AVERAGE HESABI
+    ##Moving average ile daha smooth bir veri elde ediliyor, ekstren değerler ihmal ediliyor.
+    dff["EWMA3"] = dff["sla.40"].ewm(span=3).mean()
+    ax.plot_date(dff["cdate_t"], dff["EWMA3"], "red", label="Hareketli Ortalama")
+
+    # Year-Month bilgileri için MonthLocator kullanılmalı
+    ax.xaxis.set_major_locator(MonthLocator(interval=18))
+    # Burada ise verinin veri tipinin formatı girilmeli
+    ax.fmt_xdata = DateFormatter('% Y-% m-% d')
+
+    # TREND EĞRİSİ HESAPLAMA VE ÇİZDİRME
+    ## İlk olarak datetime objectler matplotlib için çevriliyor
+    x_date = dff["cdate_t"]
+    x_num = dates.date2num(x_date)
+    ##Trend eğrisi hesaplanıyor. Birinci dereceden yapılıyor.
+    trend = np.polyfit(x_num, dff["sla.40"], 1)
+    fit = np.poly1d(trend)
+    ##Veriler tekrar datetime'a çevriliyor
+    x_fit = np.linspace(x_num.min(), x_num.max())
+    ax.plot_date(dates.num2date(x_fit), fit(x_fit), "k--", label="Trend")
+
+    # MEAN SQUARE ERROR HESABI
+    ##Ortalama SSH değeri
+    ort_sla = dff["sla.40"].mean()
+    ort_sla = "%.3f" % ort_sla
+    ##Ortalama Karesel Hata Hesabı (MSE)
+    mse = rmn.mean_square_aylik_lrm(df)
+    mse = "%.2f" % mse
+
+    # Trend değerlerinin plota yazdırılması
+    plt.text(datetime.date(2002, 1, 1), -0.18, f"Trend: {ort_sla} m ± {mse} mm/yıl")
+
+    ax.set_xlabel("Tarih", fontsize=13)
+    ax.set_ylabel("Ortalama Aylık Deniz Seviyesi Anomalisi (m)", fontsize=13)
+    ax.legend(loc="best")
+    plt.grid(True)
+    plt.title(title)
+    plt.show()
+#-----------------------------------------------------------------------------------------------------------------------
+def sla_yillik(df, title):
+    """
+        --> Bu fonksiyonun amacı girdi olarak girilen herhangi bir SLA df'inin SLA plotunu çizdirmektir.
+        --> Yıllık fonksiyonu ile elde edilen veriler için geçerli.
+        --> Önceki fonksiyonlarda datetime object değiştirildiği için integer olarak kullanılıyor.
+
+            input: dataframe
+            output: plot
+        """
+
+    #İlk önce cdate_t değerleri indexe gittiği için ve çizilirken sorun yaşandığı için index resetlenir
+    dfx = df.reset_index()
+    #Ardından dfx çizdirilirken sorun verdiği için dff diye yeni bir dataframe e kopyalanır
+    dff = dfx
+
+    #Plotun çizdirilmesi
+    fig, ax = plt.subplots()
+    ax.locator_params(integer=True)
+    ax.plot(dff["cdate_t"], dff["sla.40"], "royalblue", label="Deniz Seviyesi Anomalisi")
+
+    # Geriye kalan bilgiler. Bu kodun bitimi ile ssh plotu çizimi biter.
+    ax.set_xlabel("Yıl", fontsize=13)
+    ax.set_ylabel("Ortalama Yıllık Deniz Seviyesi Anomalisi (m)", fontsize=13)
+
+    plt.title(title)
+    plt.legend(loc = "best")
+    plt.grid(True)
+    plt.show()
+
+def iki_df_sla_plot(df1, df2, title, mod):
+
+    """
+        --> İki farklı SLA df'inin plotunu çizdirmeyi sağlar.
+        --> Mod seçeneği üç çeşittir: Gunluk SLA, Aylık SLA ve Yıllık SLA'dır.
+        --> df1: lrm, df2: ales
+    """
+
+    if mod == "gunluk":
+
+        # LRM ve ALES verileri ile figürün çizdirilmesi
+        fig, ax = plt.subplots()
+        ax.yaxis.set_major_formatter(FormatStrFormatter('%.2f'))
+        ax.plot_date(df1["cdate_t"], df1["sla.40"], "royalblue", label="LRM/SAR/SARIN Deniz Seviyesi Anomalisi")
+        ax.plot_date(df2["cdate_t"], df2["sla"], "red", label="ALES Deniz Seviyesi Anomalisi")
+
+        # Sadece year bilgileri için YearLocator kullanılmalı
+        ax.xaxis.set_major_locator(YearLocator())
+        # Burada ise verinin veri tipinin formatı girilmeli
+        ax.fmt_xdata = DateFormatter('% Y-% m-% d')
+
+        # Geriye kalan bilgiler. Bu kodun bitimi ile ssh plotu çizimi biter.
+        ax.set_xlabel("Yıl", fontsize=13)
+        ax.set_ylabel("Günlük Deniz Seviyesi Anomalisi (m)", fontsize=13)
+
+        ##Ortalama SLA değeri
+        ort_ssh_lrm = df1["sla.40"].mean()
+        ort_ssh_lrm = "%.3f" % ort_ssh_lrm
+
+        ort_ssh_ales = df2["sla"].mean()
+        ort_ssh_ales = "%.3f" % ort_ssh_ales
+
+        ##Ortalama Karesel Hata Hesabı (MSE)
+        mse_lrm = rmn.mean_square_gunluk_lrm(df1)
+        mse_lrm = "%.2f" % mse_lrm
+
+        mse_ales = rmn.mean_square_gunluk(df2)
+        mse_ales = "%.2f" % mse_ales
+
+        # Ortalama değerlerinin plota yazdırılması
+        plt.text(datetime.date(2017, 1, 1), -0.54, f"LRM/SAR/SARIN Trend: {ort_ssh_lrm} m ± {mse_lrm} mm/yıl")
+        plt.text(datetime.date(2017, 1, 1), -0.58, f"ALES Trend: {ort_ssh_ales} m ± {mse_ales} mm/yıl")
+
+        plt.title(title)
+        ax.legend(loc="best")
+        plt.grid(True)
+        plt.show()
+
+    elif mod == "aylik":
+
+        # İlk önce cdate_t değerleri indexe gittiği için ve çizilirken sorun yaşandığı için index resetlenir
+        dfx1 = df1.reset_index()
+        dfx2 = df2.reset_index()
+
+        # Ardından dfx çizdirilirken sorun verdiği için dff diye yeni bir dataframe e kopyalanır
+        dff1 = dfx1
+        dff2 = dfx2
+
+        # Nan değerlerinin alınmaması
+        dff1 = dff1[dff1["sla.40"].notna()]
+        dff2 = dff2[dff2["sla"].notna()]
+
+        # Plotun çizdirilmesi
+        fig, ax = plt.subplots()
+        ax.yaxis.set_major_formatter(FormatStrFormatter('%.2f'))
+        ax.plot_date(dff1["cdate_t"], dff1["sla.40"], "royalblue", label="LRM/SAR/SARIN Deniz Seviyesi Anomalisi")
+        ax.plot_date(dff2["cdate_t"], dff2["sla"], "red", label="ALES Deniz Seviyesi Anomalisi")
+
+        # Year-Month bilgileri için MonthLocator kullanılmalı
+        ax.xaxis.set_major_locator(MonthLocator(interval=18))
+        # Burada ise verinin veri tipinin formatı girilmeli
+        ax.fmt_xdata = DateFormatter('% Y-% m-% d')
+
+        ##Ortalama SLA değeri
+        ort_ssh_lrm = dff1["sla.40"].mean()
+        ort_ssh_lrm = "%.3f" % ort_ssh_lrm
+
+        ort_ssh_ales = dff2["sla"].mean()
+        ort_ssh_ales = "%.3f" % ort_ssh_ales
+
+        ##Ortalama Karesel Hata Hesabı (MSE)
+        mse_lrm = rmn.mean_square_aylik_lrm(df1)
+        mse_lrm = "%.2f" % mse_lrm
+
+        mse_ales = rmn.mean_square_aylik(df2)
+        mse_ales = "%.2f" % mse_ales
+
+        ax.set_xlabel("Tarih (Yıl-Ay)", fontsize=13)
+        ax.set_ylabel("Ortalama Aylık Deniz Seviyesi Anomalisi (m)", fontsize=13)
+        ax.legend(loc="best")
+        plt.grid(True)
+        plt.title(title)
+
+        # Ortalama değerlerinin plota yazdırılması
+        plt.text(datetime.date(2017, 6, 1), -0.37, f"LRM/SAR/SARIN Trend: {ort_ssh_lrm} m ± {mse_lrm} mm/yıl")
+        plt.text(datetime.date(2017, 6, 1), -0.42, f"ALES Trend: {ort_ssh_ales} m ± {mse_ales} mm/yıl")
+
+        plt.show()
+
+    elif mod == "yillik":
+
+        # İlk önce cdate_t değerleri indexe gittiği için ve çizilirken sorun yaşandığı için index resetlenir
+        dfx1 = df1.reset_index()
+        dfx2 = df2.reset_index()
+
+        # Ardından dfx çizdirilirken sorun verdiği için dff diye yeni bir dataframe e kopyalanır
+        dff1 = dfx1
+        dff2 = dfx2
+
+        # Plotun çizdirilmesi
+        fig, ax = plt.subplots()
+        ax.locator_params(integer=True)
+        ax.plot(dff1["cdate_t"], dff1["sla.40"], "royalblue", label="LRM/SAR/SARIN Deniz Seviyesi Anomalisi")
+        ax.plot_date(dff2["cdate_t"], dff2["sla"], "red", label="ALES Deniz Seviyesi Anomalisi")
+
+        # Geriye kalan bilgiler. Bu kodun bitimi ile ssh plotu çizimi biter.
+        ax.set_xlabel("Yıl", fontsize=13)
+        ax.set_ylabel("Ortalama Yıllık Deniz Seviyesi Anomalisi (m)", fontsize=13)
+
+        ##Ortalama SLA değeri
+        ort_ssh_lrm = df1["sla.40"].mean()
+        ort_ssh_lrm = "%.3f" % ort_ssh_lrm
+
+        ort_ssh_ales = df2["sla"].mean()
+        ort_ssh_ales = "%.3f" % ort_ssh_ales
+
+        # Ortalama değerlerinin plota yazdırılması
+        plt.text(2018, 0, f"Trend: {ort_ssh_lrm} m")
+        plt.text(2018, 0, f"Trend: {ort_ssh_ales} m")
+
+        plt.title(title)
+        plt.legend(loc="best")
+        plt.grid(True)
+        plt.show()
+
+
+
+
--- read_merge_nc.py
+++ read_merge_nc.py
@@ -9,6 +9,8 @@ import cartopy as ca
 import os
 import juliandate as jd
 from datetime import datetime
+import math as m
+from dateutil.relativedelta import relativedelta
 
 #Dosyaların okunarak dataframe haline getirilmesi
 
@@ -180,7 +182,7 @@ def yuks_time_ilk_degerler(df):
         input: dataframe
         output: dataframe
     """
-    yeni_df = df.iloc[df.index == 2]
+    yeni_df = df.iloc[df.index == 3]
     return yeni_df
 
 def yuks_time_son_degerler(df):
@@ -198,13 +200,8 @@ def yuks_time_son_degerler(df):
         input: dataframe
         output: dataframe
         """
-    yeni_df = df.iloc[df.index == 8]
+    yeni_df = df.iloc[df.index == 4]
     return yeni_df
-
-def ort_sla_hesapla(df):
-
-    a = df["sla"].mean()
-    return a
 
 def distance_filter(df):
     """
@@ -218,13 +215,297 @@ def distance_filter(df):
     df_result = df[df["distance.00"] < 12]
     return df_result
 
+def igneada_icin(df):
+    """
+        --> Bu fonkisyonun amacı karmakarışık olan ölçülerin filtrelenerek minimum değerlerinin alınması.
 
+        input: df
+        output: df
+    """
 
+    df_result = df[df["ssh.55"] > 37]
+    return df_result
 
+def merge_nc_sla(files):
+    """
+        --> Bu fonksiyonun ana amacı indirilen SLA nc dosyalarının path'inin fonksiyona girdi olarak girilmesidir.
+        --> Ardından pathteki dosyalar sıralanarak ilk olarak xarray kütüphanesinin datasetine aktarılır.
+        --> Ardından da hepsi dataframe olması için birer liste olur ve bunlar concat edilerek merge
+    edilmiş bir dataframe elde edilir.
+        --> Ardından BC 4713'e göre jday.00 değerleri düzenlenir. Bunun için yeni bir column'a bu değerler atanır.
+        --> Ardından gregoryan tarihlerinin elde edilmesi için cdate adında yeni bir columnd oluşturulur ve jday
+    tarihleri calendar date'e çevrilir.
+        --> Fakat elde edilen "cdate" değerleri de tuple'lar içerisinde kalır. Bundan dolayı yeni bir liste
+    oluşturulur ve yazılan for loopu ile "cdate_2" adı verilen ve sadece yıl-ay-gün bilgilerini içeren
+    yeni bir liste elde edilir.
+        --> Son olarak bu veriler dataframe'e aktarılarak sonuç ürün elde edilir.
 
+        input: path
+        output: dataframe
+        """
+    paths = sorted(glob.glob(files))
+    datasets = [xr.open_dataset(p) for p in paths]
+    dataframes = [p.to_dataframe() for p in datasets]
+    dfs = pd.concat(dataframes)
 
+    # Julian günü değişim
+    # 1 Ocak 2000 12:00 UTM günü Julian günü olarak (4713 BC'e göre) 2451545.00000 gününe denk gelmekte.
+    # Elimdeki julian günlerini bu date ile toplarsam aslında epok kaydırma yapmış olurum, bu sayede BC 4713'e göre tarih bulurum.
+    # Ardından yeni epokları gregoryana çevirebilirim
 
+    jday4713 = [i + 2451545 for i in dfs["jday.00"]]
+    dfs.insert(loc = 7, column="jday.4713", value=jday4713)
 
+    # Calendar date'e çevirme
+    cdate = [jd.to_gregorian(i) for i in dfs["jday.4713"]]
+    dfs.insert(loc = 8, column="cdate", value=cdate)
+
+    # Datetime'a çevirme
+    cdate_2 = []
+
+    for i in cdate:
+        dt_obj = datetime(*i)
+        x = dt_obj.strftime("%Y-%m-%d")
+        cdate_2.append(x)
+    dfs.insert(loc = 9, column="cdate_t", value=cdate_2)
+
+    dfs["cdate_t"] = pd.to_datetime(dfs["cdate_t"])
+
+    return dfs
+
+def aylik_sla(df):
+    """
+        --> Bu fonkisyonun amacı girdi olarak girilen bir SLA dataframedeki değerlerin tarihlere göre
+    ortalamasının alınmasıdır.les_sla_filter
+        --> Ortalamalar alınarak aylık değerler elde edilecektir.
+
+        input: dataframe
+        output: dataframe
+    """
+
+    df_aylık = df.resample("MS", on="cdate_t").mean()
+    return df_aylık
+
+def yillik_sla(df):
+    """
+        --> Bu fonkisyonun amacı girdi olarak girilen bir dataframedeki değerlerin tarihlere göre
+    ortalamasının alınmasıdır.
+        --> Ortalamalar alınarak aylık değerler elde edilecektir.
+
+        input: dataframe
+        output: dataframe
+        """
+
+    df_yillik = df.groupby(df['cdate_t'].dt.year).mean()
+    return df_yillik
+
+def sifir_index(df):
+    yeni_df = df.iloc[df.index == 0]
+    return yeni_df
+
+def bir_index(df):
+    yeni_df = df.iloc[df.index == 1]
+    return yeni_df
+
+def iki_index(df):
+    yeni_df = df.iloc[df.index == 2]
+    return yeni_df
+
+def uc_index(df):
+    yeni_df = df.iloc[df.index == 3]
+    return yeni_df
+
+def dort_index(df):
+    yeni_df = df.iloc[df.index == 4]
+    return yeni_df
+
+def bes_index(df):
+    yeni_df = df.iloc[df.index == 5]
+    return yeni_df
+
+def alti_index(df):
+    yeni_df = df.iloc[df.index == 6]
+    return yeni_df
+
+def yedi_index(df):
+    yeni_df = df.iloc[df.index == 7]
+    return yeni_df
+
+def sekiz_index(df):
+    yeni_df = df.iloc[df.index == 8]
+    return yeni_df
+
+def dokuz_index(df):
+    yeni_df = df.iloc[df.index == 9]
+    return yeni_df
+
+def on_index(df):
+    yeni_df = df.iloc[df.index == 10]
+    return yeni_df
+
+def ondort_index(df):
+    yeni_df = df.iloc[df.index == 14]
+    return yeni_df
+
+def ales_sla_filter(df):
+    """
+        --> SLA'lara göre noiselu verileri elimine etmek için
+    :param df:
+    :return:
+        """
+    df_result = df[df["sla"] < 0.7]
+    df_result = df_result[df_result["sla"] > -0.7]
+    return df_result
+
+def lrm_sla_filter(df):
+    """
+        --> SLA'lara göre noiselu verileri elimine etmek için
+    :param df:
+    :return:
+    """
+    df_result = df[df["sla.40"] < 0.7]
+    df_result = df_result[df_result["sla.40"] > -0.7]
+    return df_result
+
+def mean_square_aylik(df):
+    """
+        --> SLA için karesel ortalama hata hesabı yapar.
+    """
+
+    #NaN değerlerin alınmaması
+    df = df[df["sla"].notna()]
+
+    #Ortalama anomali hesabı
+    ort_sla = df["sla"].mean()
+
+    # v = x - l eşitliği için liste
+    v_list = []
+
+    for i in df.sla:
+        v = ort_sla - i
+        v_list.append(v)
+        i += 1
+
+    # v lerin kareleri
+    v_list_kare = []
+
+    for i in v_list:
+        v_kare = i * i
+        v_list_kare.append(v_kare)
+        i += 1
+
+    differences_in_years = relativedelta(df.index[-1], df.index[0]).years
+
+    # Karesel ortalama hata hesabı
+    mse = m.sqrt(sum(v_list_kare) / len(v_list_kare))#m
+    mse = mse / differences_in_years #m
+    mse = mse * 1000 #mm
+    return mse
+
+def mean_square_gunluk(df):
+    """
+        --> SLA için karesel ortalama hata hesabı yapar.
+    """
+
+    #NaN değerlerin alınmaması
+    df = df[df["sla"].notna()]
+
+    #Ortalama anomali hesabı
+    ort_sla = df["sla"].mean()
+
+    # v = x - l eşitliği için liste
+    v_list = []
+
+    for i in df.sla:
+        v = ort_sla - i
+        v_list.append(v)
+        i += 1
+
+    # v lerin kareleri
+    v_list_kare = []
+
+    for i in v_list:
+        v_kare = i * i
+        v_list_kare.append(v_kare)
+        i += 1
+
+    differences_in_years = relativedelta(df["cdate_t"].iat[-1], df["cdate_t"].iat[0]).years
+
+    # Karesel ortalama hata hesabı
+    mse = m.sqrt(sum(v_list_kare) / len(v_list_kare))#m
+    mse = mse / differences_in_years #m
+    mse = mse * 1000 #mm
+    return mse
+
+def mean_square_gunluk_lrm(df):
+    """
+        --> SLA için karesel ortalama hata hesabı yapar.
+    """
+
+    #NaN değerlerin alınmaması
+    df = df[df["sla.40"].notna()]
+
+    #Ortalama anomali hesabı
+    ort_sla = df["sla.40"].mean()
+
+    # v = x - l eşitliği için liste
+    v_list = []
+
+    for i in df["sla.40"]:
+        v = ort_sla - i
+        v_list.append(v)
+        i += 1
+
+    # v lerin kareleri
+    v_list_kare = []
+
+    for i in v_list:
+        v_kare = i * i
+        v_list_kare.append(v_kare)
+        i += 1
+
+    differences_in_years = relativedelta(df["cdate_t"].iat[-1], df["cdate_t"].iat[0]).years
+
+    # Karesel ortalama hata hesabı
+    mse = m.sqrt(sum(v_list_kare) / len(v_list_kare))#m
+    mse = mse / differences_in_years #m
+    mse = mse * 1000 #mm
+    return mse
+
+def mean_square_aylik_lrm(df):
+    """
+        --> SLA için karesel ortalama hata hesabı yapar.
+    """
+
+    #NaN değerlerin alınmaması
+    df = df[df["sla.40"].notna()]
+
+    #Ortalama anomali hesabı
+    ort_sla = df["sla.40"].mean()
+
+    # v = x - l eşitliği için liste
+    v_list = []
+
+    for i in df["sla.40"]:
+        v = ort_sla - i
+        v_list.append(v)
+        i += 1
+
+    # v lerin kareleri
+    v_list_kare = []
+
+    for i in v_list:
+        v_kare = i * i
+        v_list_kare.append(v_kare)
+        i += 1
+
+    differences_in_years = relativedelta(df.index[-1], df.index[0]).years
+
+    # Karesel ortalama hata hesabı
+    mse = m.sqrt(sum(v_list_kare) / len(v_list_kare))#m
+    mse = mse / differences_in_years #m
+    mse = mse * 1000 #mm
+    return mse
 
 
 

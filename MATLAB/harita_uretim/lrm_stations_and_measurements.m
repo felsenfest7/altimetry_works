976c838b776dfef4145ff635165265f998cd092b
felsenfest7 <92101782+felsenfest7@users.noreply.github.com>
2023-02-20 17:07:12 +0300

    Add files via upload

---
+++ MATLAB/harita_uretim/lrm_stations_and_measurements.m
@@ -0,0 +1,26 @@
+clc, clear, close all
+
+%Veriler
+gunluk_data = readtable("/home/furkan/deus/ALTIMETRY/processler/tum_gunluk_lrm_verileri.xlsx");
+aylik_data = readtable("/home/furkan/deus/ALTIMETRY/processler/tum_aylik_lrm_verileri.xlsx");
+
+%TUDES istasyonlarına ait bilgier
+istasyonlar = readtable("/home/furkan/deus/ALTIMETRY/processler/istasyonlarin_konumlari.xlsx");
+
+%Verilerin çizdirilmesi
+figure;
+geoscatter(istasyonlar.Latitude, istasyonlar.Longitude, "filled", "^", "MarkerEdgeColor", "r",...
+           "MarkerFaceColor", "r", "DisplayName", "TUDES İstasyonları");
+
+hold on
+
+geoscatter(aylik_data.glat_00, aylik_data.glon_00, "filled", "o", "MarkerEdgeColor", "c", ...
+           "MarkerFaceColor","c", "DisplayName", "Aylık LRM/SAR Verisi");
+geobasemap satellite;
+
+title("Aylık Ortalama LRM/SAR Verileri ile TUDES İstasyonlarının Koordinatları");
+legend();
+
+%İstasyonların isimlerinin haritaya yazdırılması
+txt = strcat(istasyonlar.Station);
+text(istasyonlar.Latitude-0.07,istasyonlar.Longitude-0.1,txt, "Color", "white", "FontWeight", "Bold");
